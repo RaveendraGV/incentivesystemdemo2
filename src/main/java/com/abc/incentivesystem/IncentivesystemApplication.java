272e071a6cbb222da19d9dd1f5002d68e4904dd8
ojagtap <ojagtap@LIN25007880.corp.capgemini.com>
2021-12-03 15:51:14 +0530

    java

--- src/main/java/com/abc/incentivesystem/IncentivesystemApplication.java
+++ src/main/java/com/abc/incentivesystem/IncentivesystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IncentivesystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("java");
 		SpringApplication.run(IncentivesystemApplication.class, args);
 	}
 
